75c1373221f8d229b9d85ad6647647f55465d09e
Shion <shion@linux>
2026-06-01 14:36:26 +0900

    修正

--- src/controller.cc
+++ src/controller.cc
@@ -657,7 +657,7 @@ controller[mc::NONLINEAR_EMS] = [](robot_system &robot)
 
   if (fp == nullptr)
   {
-    const std::string data_dir = "../data/2026_06_01";
+    const std::string data_dir = "../data/2026_06_01"; //////////////////////////////////////////////////////////
 
     mkdir(data_dir.c_str(), 0755);
 
@@ -925,7 +925,7 @@ if (count >= update_interval_count)
       if (record_count <= 0) record_count = 10;
       if (final_step_index < 0) final_step_index = 0;
 
-      const std::string data_dir = "../data/2026_06_01";
+      const std::string data_dir = "../data/2026_06_01";/////////////////////////////////////////////////////////////////////
 
       mkdir(data_dir.c_str(), 0755);////c言語形式の文字列，権限
       const std::string file_path = data_dir + "/" + record_file_name;
@@ -1106,7 +1106,7 @@ if (count >= update_interval_count)
       if (record_end_time <= 0.0) record_end_time = initial_zero_time + 1.0;
       if (record_count <= 0) record_count = 10;
 
-      const std::string data_dir = "../data/2026_06_01";/////////////////////////////////////////////////////
+      const std::string data_dir = "../data/2026_06_01";////////////////////////////////////////////////////////////////////
       mkdir(data_dir.c_str(), 0755);
       const std::string file_path = data_dir + "/" + record_file_name;
       fp = fopen(file_path.c_str(), "w");
